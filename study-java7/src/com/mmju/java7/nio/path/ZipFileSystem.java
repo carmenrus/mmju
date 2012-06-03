859b5cd7d007c3a3d125e3c1433abeeda0a36077
minlwin <lwin.zawmin@gmail.com>
2012-06-03 13:02:23 +0900

    nio

---
+++ study-java7/src/com/mmju/java7/nio/path/ZipFileSystem.java
@@ -0,0 +1,23 @@
+package com.mmju.java7.nio.path;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipFileSystem {
+
+	public static void main(String[] args) {
+
+		Path zip = Paths.get("A:", "var", "tomcat-7", "lib", "catalina.jar");
+
+		try (FileSystem fs = FileSystems.newFileSystem(zip,
+				ClassLoader.getSystemClassLoader())) {
+			System.out.println("FileSystem : " + fs);
+			System.out.println("Class : " + fs.getClass());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
