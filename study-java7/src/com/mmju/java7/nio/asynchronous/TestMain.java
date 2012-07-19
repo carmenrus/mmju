e6f35c99e4c254745c172ef94bdcfa0f2b745836
minlwin <lwin.zawmin@gmail.com>
2012-07-19 12:44:37 +0900

    TestMain

--- study-java7/src/com/mmju/java7/nio/asynchronous/TestMain.java
+++ study-java7/src/com/mmju/java7/nio/asynchronous/TestMain.java
@@ -19,25 +19,25 @@ public class TestMain {
 			byte[] msg = new byte[data.length];
 
 			socket = new Socket(server, servPort);
-			System.out.println("サーバとの接続を確立。");
+			System.out.println("Connection To Server");
 
 			InputStream in = socket.getInputStream();
 			OutputStream out = socket.getOutputStream();
 
-			out.write(data); // サーバに文字列を送付
-			System.out.println("送信：" + new String(data));
+			out.write(data);
+			System.out.println("Send：" + new String(data));
 
-			// サーバからの返信を受信
+			// get Server Message
 			int totalBytesRcvd = 0;
 			int bytesRcvd;
-			while (totalBytesRcvd < data.length) { // 全文を受信するまでloop
-				if ((bytesRcvd = in.read( // 引数は読込データ、Offset、読込データ長
+			while (totalBytesRcvd < data.length) { 
+				if ((bytesRcvd = in.read( 
 						msg, totalBytesRcvd, data.length - totalBytesRcvd)) == -1) {
-					throw new SocketException("接続遮断");
+					throw new SocketException("Fail");
 				}
 				totalBytesRcvd += bytesRcvd;
 			} // while end
-			System.out.println("受信：" + new String(msg));
+			System.out.println("Recieve：" + new String(msg));
 
 			socket.close();
 			
